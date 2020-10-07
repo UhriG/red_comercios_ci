a0316dfb27ad222e4769a2d89f3eb8dd8ba51086
carganet <carganet.cristian@gmail.com>
2020-10-07 08:58:19 -0300

    maquetado del formulario de registro

--- application/views/register.php
+++ application/views/register.php
@@ -1,58 +1,119 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <title>Registro</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 3 | Registration Page</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?=base_url('assets/');?>plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?=base_url('assets/');?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?=base_url('assets/');?>dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
-<body>
-    <h1>Registro</h1>
-    <ul>
-		<?php foreach ($menu as $item): ?>
-			<li><a href="<?= $item['url']; ?>"><?= $item['title']; ?></a></li>
-		<?php endforeach; ?>
-	</ul>
 
-	<?php echo validation_errors(); ?>
+<body class="hold-transition register-page">
+    <div class="register-box">
+        <div class="register-logo">
+            <a href="<?=base_url();?>"><b>Admin</b>LTE</a>
+        </div>
 
-    <?php 
-        echo form_open('register/create',array('method'=>'POST'));
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="login-box-msg">Formulario de registro</p>
+                <?php echo validation_errors(); ?>
 
-        echo form_label('Nombre:');
-        echo form_input('firstname');
+                <?= form_open('register/create',array('method'=>'POST'));?>
+                <div class="input-group mb-3">
+                    <input type="text" name="firstname" value="" class="form-control" placeholder="Nombre">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="text" name="lastname" value="" class="form-control" placeholder="Apellido">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="text" name="dni" value="" class="form-control" placeholder="DNI">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="email" name="email" value="" class="form-control" placeholder="Email">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="tel" name="tel" value="" class="form-control" placeholder="Teléfono">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" name="password" class="form-control" placeholder="Contraseña">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" name="password_c" class="form-control" placeholder="Reescribir contraseña">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <a href="<?=base_url('login');?>" class="text-center">Ya tengo una cuenta</a>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-6">
+                        <button type="submit" name="submit" value=""
+                            class="btn btn-primary btn-block">Registrarme</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                </form>
 
-        echo "<br>";
-        echo form_label('Apellido:');
-        echo form_input('lastname');
+            </div>
+            <!-- /.form-box -->
+        </div><!-- /.card -->
+    </div>
+    <?= isset($msg) ? $msg : ''; ?>
+    <!-- /.register-box -->
 
-        echo "<br>";
-        echo form_label('DNI:');
-        echo form_input(array('type'=>'number', 'name'=>'dni'));
-
-        echo "<br>";
-        echo form_label('Email:');
-        echo form_input(array('type' => 'email', 'name'=>'email'));
-
-        echo "<br>";
-        echo form_label('Teléfono:');
-		echo form_input(array('type' => 'tel', 'name'=>'tel'));
-		
-		echo "<br>";
-        echo form_label('Contraseña:');
-		echo form_input(array('type' => 'password', 'name'=>'password'));
-		
-		echo "<br>";
-        echo form_label('Confirmar contraseña:');
-        echo form_input(array('type' => 'password', 'name'=>'password_c'));
-
-        echo "<br>";
-        echo form_submit('submit', 'Enviar');
-
-        echo form_close();
-    ?>
-
-	<?= isset($msg) ? $msg : ''; ?>
-
+    <!-- jQuery -->
+    <script src="<?=base_url('assets/');?>plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?=base_url('assets/');?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?=base_url('assets/');?>dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
